f068e3237b08887257abb405706bf0f7482b60ac
antonMihalev <mihalev.ant@gmail.com>
2017-06-28 17:50:52 +0300

    pilot version 0.0.1

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+  $(".menu_toggle__burger").on('click',function(){
+    $(".modal").toggle('slow');
+    
+    if (!$(".modal").find(".nav__bottom").length){
+      $(".nav__bottom").clone().appendTo(".modal");
+      $(".nav__top").clone().appendTo(".modal");
+      $(".modal__cart").clone().appendTo(".modal");
+      $(".header__social").clone().appendTo(".modal");
+    }
+
+  });
+});
